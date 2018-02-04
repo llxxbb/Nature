33aa7a4a31d0bfce463a19836751fde767047448
llxxbb <llxxbb@yeah.net>
2018-02-04 16:41:36 +0800

    add hyper

--- src/bin/world_connection.rs
+++ src/bin/world_connection.rs
@@ -1,16 +1,30 @@
+extern crate hyper;
+
 #[macro_use]
 extern crate log;
 extern crate fern;
 extern crate world_connection;
 extern crate chrono;
 
-use world_connection::server::{Server};
+use world_connection::server::Server;
+use world_connection::rpc::WebServer;
+use hyper::server::{Http};
+
 
 fn main() {
     setup_logger();
     // create server
-    let server = Server{};
-    info!("Server Created");
+    let server = Server {};
+    info!("##### Server created ---------------------------");
+    // create rpc server
+    web_rpc();
+}
+
+fn web_rpc() {
+    let addr = "127.0.0.1:3000".parse().unwrap();
+    let web_server = Http::new().bind(&addr, || Ok(WebServer)).unwrap();
+    info!("##### Web service created ---------------------------");
+    web_server.run().unwrap();
 }
 
 fn setup_logger() -> Result<(), fern::InitError> {
@@ -25,6 +39,8 @@ fn setup_logger() -> Result<(), fern::InitError> {
             ))
         })
         .level(log::LevelFilter::Debug)
+        .level_for("tokio_core", log::LevelFilter::Error)
+        .level_for("hyper", log::LevelFilter::Error)
         .chain(std::io::stdout())
         .chain(fern::log_file("output.log")?)
         .apply()?;
