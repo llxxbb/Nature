e357e5e00418f0c1973c65c3e899c0f3037074eb
llxxbb <llxxbb@yeah.net>
2018-02-07 20:14:43 +0800

    test web json

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -1,9 +1,11 @@
 ///! rpc server, collect data from different rpc client then call the server
 extern crate hyper;
 extern crate futures;
+extern crate serde_json;
 
 use self::futures::future::Future;
-
+use self::futures::Stream;
+use self::hyper::{Method, StatusCode};
 use self::hyper::header::ContentLength;
 use self::hyper::server::{Http, Request, Response, Service};
 
@@ -17,29 +19,38 @@ pub fn web_rpc(port: &str) {
 
 pub struct WebServer;
 
-const PHRASE: &'static str = "Hello, World!";
+static MISSING: &[u8] = b"Missing field";
 
-impl Service for WebServer{
-
-    // boilerplate hooking up hyper's server types
+impl Service for WebServer {
     type Request = Request;
     type Response = Response;
     type Error = hyper::Error;
-    // The future representing the eventual Response your call will
-    // resolve to. This can change to whatever Future you need.
     type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
 
     fn call(&self, req: Request) -> Self::Future {
-        // We're currently ignoring the Request
-        // And returning an 'ok' Future, which means it's ready
-        // immediately, and build a Response with the 'PHRASE' body.
-        match req {
-            _ => {}
+        match (req.method(), req.path()) {
+            (&Method::Post, "/input") => {
+                Box::new(req.body().concat2().map(|b| {
+                    let json: serde_json::Value = if let Ok(j) = serde_json::from_slice(b.as_ref()) {
+                        j
+                    } else {
+                        return Response::new()
+                            .with_status(StatusCode::BadRequest)
+                            .with_header(ContentLength(MISSING.len() as u64))
+                            .with_body(MISSING);
+                    };
+                    // Validate the request parameters, returning
+                    // early if an invalid input is detected.
+
+                    let body = format!("Hello {}, your number is {}", json["name"], json["number"]);
+                    Response::new()
+                        .with_header(ContentLength(body.len() as u64))
+                        .with_body(body)
+                }))
+            }
+            _ => {
+                Box::new(futures::future::ok(Response::new().with_status(StatusCode::NotFound)))
+            }
         }
-        Box::new(futures::future::ok(
-            Response::new()
-                .with_header(ContentLength(PHRASE.len() as u64))
-                .with_body(PHRASE)
-        ))
     }
 }
