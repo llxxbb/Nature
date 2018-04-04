7603c99cf145ffd58e9b46caab90aefb9c2ad7d9
llxxbb <llxxbb@yeah.net>
2018-04-04 10:42:39 +0800

    remove all errors and warnings

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -11,7 +11,7 @@ use std::sync::Mutex;
 pub type CONN = SqliteConnection;
 
 lazy_static! {
-        pub static ref  POOL :Pool<ConnectionManager<CONN>> = create_pool::<SqliteConnection>();
+        pub static ref  POOL :Pool<ConnectionManager<CONN>> = create_pool::<CONN>();
         pub static ref  DEFINE_DAO : Mutex<ThingDefineDaoService>  =  Mutex::new(ThingDefineDaoService::new());
         pub static ref  INSTANCE_DAO : InstanceDaoService  =  InstanceDaoService{};
     }
