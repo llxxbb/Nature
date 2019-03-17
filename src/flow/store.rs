057942fe6b7bc1c3791040116181fe623ecaabaa
llxxbb <llxxbb@yeah.net>
2019-03-17 16:59:15 +0800

    add plantuml

--- src/flow/store.rs
+++ src/flow/store.rs
@@ -31,13 +31,14 @@ impl StoreServiceTrait for StoreServiceImpl {
         let uuid = self.svc_instance.verify(&mut instance)?;
         let task = self.generate_store_task(&instance)?;
         let carrier = RawTask::new(&task, &instance.thing.key, TaskType::Store as i16)?;
+        self.task_dao.insert(&carrier)?;
         self.do_task(&task, &carrier)?;
         Ok(uuid)
     }
 
     fn self_route(&self, instance: SelfRouteInstance) -> Result<u128> {
         if instance.converter.is_empty() {
-            return Err(NatureError::VerifyError("converter must not empty!".to_string()));
+            return Err(NatureError::VerifyError("converter must not empty for dynamic convert!".to_string()));
         }
         // Convert a Self-Route-Instance to Normal Instance
         let mut ins = Instance {
@@ -103,5 +104,70 @@ impl StoreServiceImpl {
             mission: Some(target),
         };
         Ok(task)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use mockers::Scenario;
+    use mockers_derive::mock;
+
+    use super::*;
+
+    mock! {
+        InstanceDaoTraitMock,
+        test,
+        trait InstanceDaoTrait{
+            fn insert(&self, instance: &Instance) -> Result<usize>;
+            /// check whether source stored earlier
+            fn is_exists(&self, instance: &Instance) -> Result<bool>;
+            fn get_by_id(&self, id: u128) -> Result<Option<Instance>>;
+            fn get_by_key(&self, key: &str, id: u128) -> Result<Option<Instance>>;
+        }
+    }
+    mock! {
+        TaskDaoTraitMock,
+        test,
+        trait TaskDaoTrait{
+            fn insert(&self, raw: &RawTask) -> Result<usize>;
+            fn delete(&self, record_id: &[u8]) -> Result<usize>;
+            fn raw_to_error(&self, err: &NatureError, raw: &RawTask) -> Result<usize>;
+            fn update_execute_time(&self, record_id: &[u8], delay: i64) -> Result<()>;
+            fn increase_times_and_delay(&self, record_id: &[u8], delay: i32) -> Result<usize>;
+            fn get(&self, record_id: &[u8]) -> Result<Option<RawTask>>;
+            fn get_overdue(&self, seconds: &str) -> Result<Vec<RawTask>>;
+        }
+    }
+    mock! {
+        InstanceServiceTraitMock,
+        test,
+        trait InstanceServiceTrait {
+            fn verify(&self, instance: &mut Instance) -> Result<u128>;
+            /// gegerate by Hash.
+            fn id_generate_if_not_set(&self, instance: &mut Instance) -> Result<u128>;
+        }
+    }
+
+    #[test]
+    fn test_input() {
+        let scenario = Scenario::new();
+        let mut mock_instance_dao = scenario.create_mock::<InstanceDaoTraitMock>();
+        let mut mock_route_svc = scenario.create_mock_for::<RouteServiceTrait>();
+        let mut mock_task_svc = scenario.create_mock_for::<TaskServiceTrait>();
+        let mut mock_task_dao = scenario.create_mock::<TaskDaoTraitMock>();
+        let mut mock_instance_svc = scenario.create_mock::<InstanceServiceTraitMock>();
+
+        let store_svc = StoreServiceImpl {
+            instance_dao: Rc::new(mock_instance_dao),
+            route: Rc::new(mock_route_svc),
+            task_svc: Rc::new(mock_task_svc),
+            task_dao: Rc::new(mock_task_dao),
+            svc_instance: Rc::new(mock_instance_svc),
+        };
+//        scenario.expect(cond.make_hotter_call(4).and_return(()));
+//
+        let instance = Instance::default();
+        scenario.expect(cond.get_temperature_call().and_return(16));
+        let rrn = store_svc.input(instance);
     }
 }
