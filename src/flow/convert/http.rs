694e44751bf6197b0fa629e841b43fc72fea5c3d
llxxbb <llxxbb@yeah.net>
2019-04-14 17:03:42 +0800

    add Http Converter support

--- src/flow/convert/http.rs
+++ src/flow/convert/http.rs
@@ -1,4 +1,6 @@
-use nature_common::{CallOutParameter, ConverterReturned};
+use reqwest::Client;
+
+use nature_common::{CallOutParameter, ConverterReturned, Instance};
 
 use crate::flow::convert::caller::ExecutorTrait;
 
@@ -10,6 +12,69 @@ pub struct HttpExecutorImpl;
 
 impl ExecutorTrait for HttpExecutorImpl {
     fn execute(&self, address: &str, para: &CallOutParameter) -> ConverterReturned {
-        unimplemented!()
+        let response = CLIENT.post(address).json(para).send();
+        match response {
+            Err(e) => {
+                warn!("failed get result from : {}, err: {:?}", address, e);
+                ConverterReturned::EnvError
+            }
+            Ok(mut res) => {
+                let result = res.json::<Vec<Instance>>();
+                match result {
+                    Err(e) => ConverterReturned::LogicalError(e.to_string()),
+                    Ok(ins) => ConverterReturned::Instances(ins)
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::thread;
+    use std::thread::JoinHandle;
+    use std::time::Duration;
+
+    use actix_web::{App, HttpRequest, Json, Result, server};
+
+    use nature_common::{CallOutParameter, ConverterReturned, Instance};
+
+    use crate::flow::convert::caller::ExecutorTrait;
+    use crate::flow::HttpExecutorImpl;
+
+    fn index(_req: &HttpRequest) -> Result<Json<Vec<Instance>>> {
+        Ok(Json(vec!(Instance::default())))
+    }
+
+    #[test]
+    fn response_ok() {
+        let _ = start_web_server();
+
+        // get result
+        let address = "http://127.0.0.1:8088";
+        let para = CallOutParameter {
+            from: Instance::default(),
+            last_status: None,
+            carrier_id: vec![],
+        };
+        let executor_impl = HttpExecutorImpl {};
+        let converted = executor_impl.execute(address, &para);
+        match converted {
+            ConverterReturned::Instances(ins) => {
+                assert_eq!(1, ins.len())
+            }
+            _ => panic!("err returned")
+        }
+    }
+
+    fn start_web_server() -> JoinHandle<()> {
+        let handler = thread::spawn(|| {
+            server::new(|| App::new().resource("/", |r| r.f(index)))
+                .bind("127.0.0.1:8088")
+                .unwrap()
+                .run();
+        });
+        thread::sleep(Duration::new(1, 0));
+        handler
     }
 }
