e6c69a7206aa7e86ac21c4f55480bea82656c25d
llxxbb <llxxbb@yeah.net>
2020-04-12 22:34:01 +0800

    fix bug: local filter crashed.

--- src/task/filter.rs
+++ src/task/filter.rs
@@ -6,10 +6,17 @@ use crate::task::local_common::local_execute;
 pub async fn filter_instance(para: &mut Vec<Instance>, filter: &Vec<Executor>) -> Result<()> {
     for f in filter {
         match f.protocol {
-            Protocol::Http => http_filter(&f.url, para).await?,
+            Protocol::Http => {
+                let _ = http_filter(&f.url, para).await;
+            }
             Protocol::LocalRust => {
-                let rtn: Result<Vec<Instance>> = local_execute(&f.url, &para).await?;
-                *para = rtn?;
+                match local_execute(&f.url, para).await {
+                    Ok(new) => match new {
+                        Ok(new) => *para = new,
+                        Err(err) => return Err(err)
+                    },
+                    Err(err) => return Err(err)
+                }
             }
             _ => return Err(NatureError::VerifyError("filter does not support this protocol".to_string()))
         }
