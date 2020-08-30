bc8f41f98931fd8159380bd6e15e8315490169eb
llxxb <llxxbb@yeah.net>
2020-08-30 19:34:37 +0800

    change sql and md file for demo

--- shell/make-release-tool.bat
+++ shell/make-release-tool.bat
@@ -5,9 +5,9 @@ if not exist %RELEASE_PATH% md %RELEASE_PATH%
 copy /Y ..\.env %RELEASE_PATH%
 copy /Y ..\target\debug\nature.exe %RELEASE_PATH%
 copy /Y ..\target\debug\retry.exe %RELEASE_PATH%
-copy /Y ..\target\debug\nature_demo_executor.dll %RELEASE_PATH%
+copy /Y ..\target\debug\nature_demo.dll %RELEASE_PATH%
 copy /Y ..\target\debug\restful_executor.exe %RELEASE_PATH%
-copy /Y ..\..\Nature-DB\doc\schema.sql %RELEASE_PATH%
+copy /Y schema.sql %RELEASE_PATH%
 
 del %RELEASE_PATH%\release.zip
 7z a -tzip %RELEASE_PATH%\release.zip %RELEASE_PATH%\nature.exe %RELEASE_PATH%\retry.exe %RELEASE_PATH%\.env %RELEASE_PATH%\schema.sql
