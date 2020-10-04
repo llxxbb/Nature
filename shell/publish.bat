efccf02b9fd166d8530a820ae0b5646ce68fe3f3
llxxbb <llxxbb@yeah.net>
2020-10-04 11:52:25 +0800

    refine publish.bat

--- shell/publish.bat
+++ shell/publish.bat
@@ -1,3 +1,4 @@
 cargo publish --no-verify --manifest-path ..\Cargo.toml
+timeout 5
 cargo publish --no-verify --manifest-path ..\..\Nature-Demo\Cargo.toml
 cargo publish --no-verify --manifest-path ..\..\Nature-Integrate-Test-Executor\Cargo.toml
